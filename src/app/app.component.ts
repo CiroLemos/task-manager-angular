15f94d4f0a84f93143d947ae5ab34c1437ba72c9
Ciro Lemos <ciro.lemos01@gmail.com>
2019-03-12 13:56:16 -0300

    adding module task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'app works!';
+  task = new Task(1,'Enviar orçamento para o usuário X');
+}
+
+export class Task {
+  public id: number;
+  public title: string;
+  
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
 }
