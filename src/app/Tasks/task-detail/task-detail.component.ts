99095f21624e2be71ebdb40d6fc908b04715db67
Ciro Lemos <ciro.lemos01@gmail.com>
2019-03-21 13:14:30 -0300

    Improving task-detail form

--- src/app/Tasks/task-detail/task-detail.component.ts
+++ src/app/Tasks/task-detail/task-detail.component.ts
@@ -43,4 +43,8 @@ export class TaskDetailComponent implements OnInit {
                 );
         }
     }
+
+    public showFieldError(field): boolean {
+        return field.invalid && (field.touched || field.dirty);
+    }
 }
