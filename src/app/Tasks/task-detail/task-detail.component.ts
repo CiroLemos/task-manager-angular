6e805587d5d00581c3f7341c3a92e5a69ddbb8a4
Ciro Lemos <ciro.lemos01@gmail.com>
2019-03-25 17:28:35 -0300

    Adding reactive forms control

--- src/app/Tasks/task-detail/task-detail.component.ts
+++ src/app/Tasks/task-detail/task-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
 import { Location } from "@angular/common";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 
 import { TaskService } from "../shared/task.service";
 import { Task } from "../shared/task.model";
@@ -11,21 +12,40 @@ import { Task } from "../shared/task.model";
 })
 export class TaskDetailComponent implements OnInit, AfterViewInit {
 
-
+    public reactiveTaskForm: FormGroup;
     public task: Task;
     public taskDoneOptions: Array<any> = [
         { value: false, text: 'Pendente' },
         { value: true, text: 'Feita' }
     ]
 
-    constructor(private taskService: TaskService, private route: ActivatedRoute, private location: Location) { }
+    constructor(private taskService: TaskService, private route: ActivatedRoute, private location: Location, private formBuilder: FormBuilder) { 
+        this.reactiveTaskForm = this.formBuilder.group({
+            title: [null],
+            deadline: [null],
+            done: [null],
+            description: [null]
+        });
+
+        //Objecto do exemplo com mais de um formulário em um formGroup.
+        // this.reactiveTaskForm = this.formBuilder.group({
+        //     title: [null],
+        //     deadline: [null],
+        //     done: [null],
+        //     description: [null],
+        //     user: this.formBuilder.group({
+        //         name: ['João Carlos'],
+        //         email: ['joaocarlos@gmail.com']
+        //     })
+        // });
+    }
 
     ngOnInit(): void {
         this.task = new Task(null, null);
         this.route.params
             .switchMap((params: Params) => this.taskService.getById(+params['id']))
             .subscribe(
-                task => this.task = task,
+                task => this.setTask(task),
                 error => alert('Ocorreu um erro no servidor'),
             );
     }
@@ -34,7 +54,12 @@ export class TaskDetailComponent implements OnInit, AfterViewInit {
         $('#deadline').datetimepicker({
             sideBySide: true,
             locale: 'pt-br'
-        }).on('dp.change', () => this.task.deadline = $('#deadline').val());
+        }).on('dp.change', () => this.reactiveTaskForm.get('deadline').setValue($('#deadline').val()));
+    }
+
+    public setTask(task: Task): void {
+        this.task = task;
+        this.reactiveTaskForm.patchValue(task);
     }
 
     public goBack() {
